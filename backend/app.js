ffcb4486ce47f8aadcd1819bd558593c9525d1dc
Abhinav Salokhe <abhinavsalokhe1433@gmail.com>
2026-05-03 00:59:09 +0530

    added be cors

--- backend/app.js
+++ backend/app.js
@@ -11,8 +11,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
-    credentials: true,
+    origin: "*",
+    credentials: false,
   })
 );
 app.use(express.json());
